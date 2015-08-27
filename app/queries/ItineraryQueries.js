16a4c6ea544ad2bbb620b7cb3f7c5b21c11045f0
sideroad <sideroad.jp@gmail.com>
2015-08-27 21:36:37 +0900

    update

--- app/queries/ItineraryQueries.js
+++ app/queries/ItineraryQueries.js
@@ -3,13 +3,16 @@
 import config           from '../config';
 import Marty            from 'marty';
 import ItineraryAPI     from '../sources/ItineraryAPI';
+import UserAPI          from '../sources/UserAPI';
 import constants        from '../constants';
 
 class ItineraryQueries extends Marty.Queries {
   getAll() {
-    return ItineraryAPI.getAll()
-      .then(res => this.dispatch(constants.ITINERARY_RECEIVE, res.body || []))
-      .catch(err => this.dispatch(constants.ITINERARY_RECEIVE_FAILED, err));
+    return UserAPI.take().then(res => 
+      ItineraryAPI.getAll(res.body.id)
+        .then(res => this.dispatch(constants.ITINERARY_RECEIVE, res.body || []))
+        .catch(err => this.dispatch(constants.ITINERARY_RECEIVE_FAILED, err))
+    );
   }
 }
 export default Marty.register(ItineraryQueries);
